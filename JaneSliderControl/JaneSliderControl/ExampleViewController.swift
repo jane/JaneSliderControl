66f17f2606d1f530ec56c8139542ad054a92c9b7
Barlow Tucker <barlowtucker@gmail.com>
2016-05-17 11:46:56 -0600

    Added some outlets to the example code

--- JaneSliderControl/JaneSliderControl/ExampleViewController.swift
+++ JaneSliderControl/JaneSliderControl/ExampleViewController.swift
@@ -15,26 +15,12 @@ class ExampleViewController: UIViewController {
     @IBOutlet weak var rightSlider: SliderControl!
     @IBOutlet weak var thinSlider: SliderControl!
     
+    @IBOutlet weak var sliderLabel: UILabel!
+    @IBOutlet weak var statusLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
