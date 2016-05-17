b9e56bdb72b2d2edef7811b58427bad0c595187d
Barlow Tucker <barlowtucker@gmail.com>
2016-05-17 10:35:00 -0600

    Initial Commit

---
+++ JaneSliderControl/JaneSliderControl/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JaneSliderControl
+//
+//  Created by Barlow Tucker on 5/17/16.
+//  Copyright © 2016 Jane. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
